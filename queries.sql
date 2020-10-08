fc61f4a4cb72b3377e3d1af98f25e788c6759373
nelson-singleton <nsingleton@ymail.com>
2020-10-07 23:52:37 -0500

    Scheme model nearly complete

--- queries.sql
+++ queries.sql
@@ -16,10 +16,12 @@ where OrderDate < '2012-08-09';
 SELECT OrderId, product.ProductName, quantity
 FROM OrderDetail
 JOIN product ON orderdetail.ProductId = product.Id
-where orderId = 10251;
+where orderId = 10251
+order by product.ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 select Id, Customer.CompanyName, Employee.LastName 
 from [order]
 join Customer ON [Order].CustomerId = Customer.Id 
-join Employee ON [Order].EmployeeId = Employee.Id;
+join Employee ON [Order].EmployeeId = Employee.Id
+;
